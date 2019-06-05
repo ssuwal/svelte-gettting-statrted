893113c19b872a72f47f991dc56a879e99492221
ssuwal <santoshsuwal@hotmail.com>
2019-06-05 09:13:27 -0400

    moved urls to array

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
+	import Greeting from './Greeting.svelte';
 	export let name;
 
 	let ExtComponent;
+	let pass2Greeting = `This is passed to greeting module js file`;
+
+	let ComponentIndex = [
+		`http://localhost:3000/components/Greetings.mjs`,
+		`./Greeting.svelte`
+	];
 
 	const loadExternal = () => {
-		import(`http://localhost:3000/components/Greetings.mjs`).then( resp => {
+		import(ComponentIndex[0]).then( resp => {
 			console.log(resp);
-			ExtComponent = resp/* .default */;
+			ExtComponent = resp.default;
 		})
+		// import(`./Greeting.svelte`).then( resp => {
+		// 	console.log(resp);
+		// 	ExtComponent = resp.default;
+		// })
 	}
 </script>
 
@@ -19,4 +30,4 @@
 
 <h1>Hello {name}!</h1>
 <button on:click={loadExternal}>Press this to release Awesomeness</button>
-<svelte:component this={ExtComponent} />
+<svelte:component this={ExtComponent} name={pass2Greeting}/>
---
+++ src/Greeting.svelte
@@ -0,0 +1,10 @@
+<style>
+    p {
+        color: blue;
+        font-size: 25px;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+</style>
+<p>
+    The is local Greeting Component
+</p>
